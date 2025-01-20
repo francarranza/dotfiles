c2ab1bb6e0f7f2a6750aa417f112a03e3b613e09
Francisco Carranza <hello@francarranza.com>
2025-01-20 00:11:35 -0300

    reverted rust changes

--- config/nvim/lua/plugins/mason.lua
+++ config/nvim/lua/plugins/mason.lua
@@ -1,28 +1,23 @@
 return {
   "williamboman/mason.nvim",
-  opts = function(_, opts)
-    opts.ensure_installed = opts.ensure_installed
-      or {
-        -- LSP
-        "json-lsp",
-        "jdtls",
+  opts = {
+    ensure_installed = {
+      -- LSP
+      "json-lsp",
+      "jdtls",
 
-        -- Formaters
-        "prettierd",
-        "yapf",
-        "tflint",
+      -- Formaters
+      "prettierd",
+      "yapf",
+      "tflint",
 
-        -- Linters
-        "eslint_d",
-        "pylint",
+      -- Linters
+      "eslint_d",
+      "pylint",
 
-        -- DAP
-        "java-debug-adapter",
-        "java-test",
-      }
-    vim.list_extend(opts.ensure_installed, { "codelldb" })
-    if diagnostics == "bacon-ls" then
-      vim.list_extend(opts.ensure_installed, { "bacon" })
-    end
-  end,
+      -- DAP
+      "java-debug-adapter",
+      "java-test",
+    },
+  },
 }
